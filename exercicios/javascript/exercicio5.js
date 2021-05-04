f8ecfa9e5ff829a5d64624d0f0ae5ef13ba7e9d6
Artur Silva <artur_antoniopw01@hotmail.com>
2021-05-04 16:50:14 -0300

    continuando...

---
+++ exercicios/javascript/exercicio5.js
@@ -0,0 +1,9 @@
+function maiorOuIgual(valor1, valor2){
+    let resultado = false
+    if(valor1 > valor2 || valor1 === valor2){
+        resultado = true
+    }
+    console.log(resultado)
+}
+
+maiorOuIgual(2, 1)
